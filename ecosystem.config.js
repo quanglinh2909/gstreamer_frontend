984219e8dcc752b836262e9a9c25ec3a38a1eff5
orangepi <hackcoquanglinh2000@gmail.com>
2026-06-10 14:08:36 +0700

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'gstreamer-fontend',
+             script: 'npm start', 
+            watch: '',
+            ignore_watch: [],
+            autorestart: true,
+
+        },
+    ],
+};
